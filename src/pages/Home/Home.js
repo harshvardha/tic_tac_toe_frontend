f096607f70133676dd499c8ff6aada4190ce13c3
harshvardhan singh <harshvardhansingh458@protonmail.com>
2024-02-24 22:19:54 +0530

    integrated it with backend

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,55 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { socket, isSocketConnected } from "../../socketConnection";
 import "./Home.css";
 
 const Home = () => {
     const [username, setUsername] = useState("");
+    const [searchingForOpponent, setSearchingForOpponent] = useState(false);
+    const navigateTo = useNavigate();
+
+    const searchForOpponent = async () => {
+        try {
+            if (isSocketConnected) {
+
+                // emit event `ready_to_play` with username to server
+                socket.emit("ready_to_play", {
+                    username: username
+                });
+
+                // 'created-room' event is recieved if no empty lobby was present so server creates on for you and waits for someone to join
+                socket.on("created-room", data => {
+                    if (data.message === "waiting for opponent") {
+                        setSearchingForOpponent(true);
+                    }
+                });
+
+                // 'game-starting' event is recieved when someone joins the lobby user created
+                socket.on("game-starting", data => {
+                    setSearchingForOpponent(false);
+                    navigateTo("/game", {
+                        state: {
+                            players: data,
+                            playerNo: 1
+                        }
+                    });
+                });
+
+                // 'joined-room' event is recieved when user joins lobby created by some other user who is waiting for a player to join
+                socket.on("joined-room", data => {
+                    setSearchingForOpponent(false);
+                    navigateTo("/game", {
+                        state: {
+                            players: data,
+                            playerNo: 2
+                        }
+                    })
+                });
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     return (
         <div className="home">
@@ -14,6 +61,7 @@ const Home = () => {
                 value={username}
                 onChange={(event) => setUsername(event.target.value)}
             />
+            {searchingForOpponent ? <p>Waiting for opponent...</p> : <button type="button" id="search_game_button" onClick={searchForOpponent}>search for game</button>}
         </div>
     )
 }
