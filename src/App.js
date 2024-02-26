99f9c9005091e794e4500dcedc1f8733539b566b
harshvardhan singh <harshvardhansingh458@protonmail.com>
2024-02-27 00:43:24 +0530

    changed path from '/home' to just '/'

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/game" element={<Game />} />
         </Routes>
       </BrowserRouter>
